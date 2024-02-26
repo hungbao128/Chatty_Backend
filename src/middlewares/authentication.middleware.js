d564273aa8f0bfc8578e634ea45cb97e0426abd8
hung tang <baohung20020112@gmail.com>
2024-02-26 20:58:57 +0700

    Fix error message

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -7,7 +7,7 @@ const { verifyToken } = require("../utils/jwt");
 const authentication = async(req, res, next) => {
     try {
         if(!req.headers.authorization){
-            throw new BadRequest('Invalid token.');
+            throw new BadRequest('Unauthorized.');
         }
 
         const token = req.headers.authorization.split(' ')[1];
@@ -16,7 +16,7 @@ const authentication = async(req, res, next) => {
 
         const user = await userService.findById(decoded.id);
         if(!user){
-            throw new BadRequest('Invalid token.');
+            throw new BadRequest('Unauthorized.');
         }
         req.user = UserHelper.generateUserResponse(user.toObject());
         next();
