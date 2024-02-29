3f6134d55cff2df83e93a18485fbbae52eea538c
hung tang <baohung20020112@gmail.com>
2024-02-29 19:20:06 +0700

    Change get current user friends route

--- src/routes/userFriend.route.js
+++ src/routes/userFriend.route.js
@@ -10,6 +10,6 @@ router.post('/request/:id', catchAsync(authentication), catchAsync(userFriendCon
 router.post('/accept/:id', catchAsync(authentication), catchAsync(userFriendController.acceptFriendRequest));
 router.post('/reject/:id', catchAsync(authentication), catchAsync(userFriendController.rejectFriendRequest));
 router.post('/cancel/:id', catchAsync(authentication), catchAsync(userFriendController.rejectFriendRequest));
-router.get('/friends', catchAsync(authentication), catchAsync(userFriendController.getCurrentUserFriends));
+router.get('/', catchAsync(authentication), catchAsync(userFriendController.getCurrentUserFriends));
 
 module.exports = router;
