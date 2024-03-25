606c066c8a9395ce033ffcd914b8d58bce7cedea
H82M19\Student <Student>
2024-03-25 13:27:53 +0700

    Show conversation image

--- src/helpers/ConservationHelper.js
+++ src/helpers/ConservationHelper.js
@@ -3,8 +3,9 @@ class ConservationHelper {
     static generateConservation(conservation, userId) {
         if(conservation.type === 'private') {
             const result = conservation.members.filter(member => member._id.toString() != userId);
+            console.log(result[0]);
             conservation.name = result[0].name;
-            conservation.image = result[0].image;
+            conservation.image = result[0].avatar;
         }
 
         return conservation;
