01f961459556a7461d52de3b6f285566d6973e0c
hungtang <clemencedoan128@gmail.com>
2024-04-02 11:10:19 +0700

    Fix mesage response

--- src/helpers/MessageHelper.js
+++ src/helpers/MessageHelper.js
@@ -1,18 +1,18 @@
 class MessageHelper {
-    static generateMessage(message, userId) {
-        const sender = message.sender;
+  static generateMessage(message, userId) {
+    const sender = message.sender;
 
-        message = message.toObject();
-        message.avatar = sender.avatar;
-        message.name = sender.name;
-        message.isMine = message.sender._id.toString() === userId;
+    message = message.toObject();
+    message.avatar = sender.avatar;
+    message.name = sender.name;
+    message.isMine = message.sender._id.toString() === userId.toString();
 
-        delete message.sender;
-        delete message.conservation;
-        delete message.__v;
-        
-        return message;
-    }
+    delete message.sender;
+    delete message.conservation;
+    delete message.__v;
+
+    return message;
+  }
 }
 
 module.exports = MessageHelper;
