025012b2d8cdc0b7deaabaee50c7d16ed57ff069
hungtang <clemencedoan128@gmail.com>
2024-04-02 15:45:34 +0700

    Add timestamp for last message

--- src/repositories/Conservation.repository.js
+++ src/repositories/Conservation.repository.js
@@ -3,45 +3,64 @@ const { ObjectId } = require("mongoose").Types;
 require("../models/Message.model");
 
 class ConservationRepository {
-    async create({creatorId, userId, type = 'private'}){
-        return await ConservationModel.create({
-            type,
-            creator: creatorId,
-            members: [creatorId, userId],
-            readStatus: {
-                [creatorId]: true,
-                [userId]: true
-            }
-        });
-    }
+  async create({ creatorId, userId, type = "private" }) {
+    return await ConservationModel.create({
+      type,
+      creator: creatorId,
+      members: [creatorId, userId],
+      readStatus: {
+        [creatorId]: true,
+        [userId]: true,
+      },
+    });
+  }
 
-    async findPrivateConservationByMembers(creatorId, userId){
-        return await ConservationModel.findOne({
-            type: 'private',
-            $and: [
-                {members: {$elemMatch: {$eq: userId}}},
-                {members: {$elemMatch: {$eq: creatorId}}}
-            ]
-        }).populate('members', "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v")
-            .populate('creator', "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v")
-            .populate('lastMessage', "-seenBy -createdAt -updatedAt -conservation -__v");
-    }
+  async findPrivateConservationByMembers(creatorId, userId) {
+    return await ConservationModel.findOne({
+      type: "private",
+      $and: [
+        { members: { $elemMatch: { $eq: userId } } },
+        { members: { $elemMatch: { $eq: creatorId } } },
+      ],
+    })
+      .populate(
+        "members",
+        "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v"
+      )
+      .populate(
+        "creator",
+        "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v"
+      )
+      .populate("lastMessage", "-seenBy -createdAt -conservation -__v");
+  }
 
-    async findConservationsByUserId(userId){
-        return await ConservationModel.find({members: {$in: [userId]}})
-        .populate('members', "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v")
-        .populate('creator', "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v")
-        .populate('lastMessage', "-seenBy -createdAt -updatedAt -conservation -__v")
-        .sort({updatedAt: -1});
-    }
+  async findConservationsByUserId(userId) {
+    return await ConservationModel.find({ members: { $in: [userId] } })
+      .populate(
+        "members",
+        "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v"
+      )
+      .populate(
+        "creator",
+        "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v"
+      )
+      .populate("lastMessage", "-seenBy -createdAt -conservation -__v")
+      .sort({ updatedAt: -1 });
+  }
 
-    async updateConservation(conservationId, data){
-        return await ConservationModel.updateOne({_id: new ObjectId(conservationId)}, data);
-    }
+  async updateConservation(conservationId, data) {
+    return await ConservationModel.updateOne(
+      { _id: new ObjectId(conservationId) },
+      data
+    );
+  }
 
-    async isUserInConservation(conservationId, userId){
-        return await ConservationModel.findOne({_id: new ObjectId(conservationId), members: {$in: [userId]}});
-    }
+  async isUserInConservation(conservationId, userId) {
+    return await ConservationModel.findOne({
+      _id: new ObjectId(conservationId),
+      members: { $in: [userId] },
+    });
+  }
 }
 
 module.exports = new ConservationRepository();
--- src/services/conservation.service.js
+++ src/services/conservation.service.js
@@ -5,34 +5,64 @@ const BadRequest = require("../core/BadRequest");
 const ConservationHelper = require("../helpers/ConservationHelper");
 
 class ConservationService {
-    async conservationPopulate(conservation){
-        return conservation.populate('members', "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v")
-            .then(result => result.populate('creator', "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v"))
-            .then(result => result.populate('lastMessage', "-seenBy -createdAt -updatedAt -conservation -__v"))
-    }
+  async conservationPopulate(conservation) {
+    return conservation
+      .populate(
+        "members",
+        "-password -createdAt -updatedAt -email -bio -dateOfBirth -gender -phone -__v"
+      )
+      .then((result) =>
+        result.populate(
+          "creator",
+          "-password -createdAt -updatedAt -bio -dateOfBirth -gender -phone -email -__v"
+        )
+      )
+      .then((result) =>
+        result.populate("lastMessage", "-seenBy -createdAt -conservation -__v")
+      );
+  }
 
-    async openConservation(creatorId, userId) {
-        if(creatorId == userId) throw new BadRequest('Cannot open conservation with yourself.');
+  async openConservation(creatorId, userId) {
+    if (creatorId == userId)
+      throw new BadRequest("Cannot open conservation with yourself.");
 
-        const existingConservation = await conservationRepository.findPrivateConservationByMembers(creatorId, userId);
-        
-        if(existingConservation) return ConservationHelper.generateConservation(existingConservation, creatorId);
-        
-        const existingUser = await userRepository.findUserById(userId);
-        if(!existingUser) throw new BadRequest('User not found.');
-        
-        const conservation = await conservationRepository.create({creatorId, userId});
+    const existingConservation =
+      await conservationRepository.findPrivateConservationByMembers(
+        creatorId,
+        userId
+      );
 
-        if(!conservation) throw new ServerErrorRequest('Cannot create conservation.');
+    if (existingConservation)
+      return ConservationHelper.generateConservation(
+        existingConservation,
+        creatorId
+      );
 
-        return ConservationHelper.generateConservation((await this.conservationPopulate(conservation)), creatorId);
-    }
+    const existingUser = await userRepository.findUserById(userId);
+    if (!existingUser) throw new BadRequest("User not found.");
 
-    async getUserConservations(userId){
-        const conservations = await conservationRepository.findConservationsByUserId(userId);
-        const result = conservations.map(el => ConservationHelper.generateConservation(el, userId));
-        return result;
-    }
+    const conservation = await conservationRepository.create({
+      creatorId,
+      userId,
+    });
+
+    if (!conservation)
+      throw new ServerErrorRequest("Cannot create conservation.");
+
+    return ConservationHelper.generateConservation(
+      await this.conservationPopulate(conservation),
+      creatorId
+    );
+  }
+
+  async getUserConservations(userId) {
+    const conservations =
+      await conservationRepository.findConservationsByUserId(userId);
+    const result = conservations.map((el) =>
+      ConservationHelper.generateConservation(el, userId)
+    );
+    return result;
+  }
 }
 
 module.exports = new ConservationService();
