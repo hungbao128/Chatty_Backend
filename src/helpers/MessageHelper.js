c39703f5907684fed6a9903c80b05ae7ea72c861
hungtang <clemencedoan128@gmail.com>
2024-04-05 09:48:34 +0700

    Quick fix message response

--- src/helpers/MessageHelper.js
+++ src/helpers/MessageHelper.js
@@ -2,17 +2,18 @@ class MessageHelper {
   static generateMessage(message, userId) {
     const sender = message.sender;
     // console.log(message?.constructor?.name === 'model');
-    message = (message?.constructor?.name === 'model') ? message.toObject() : message;
+    message =
+      message?.constructor?.name === "model" ? message.toObject() : message;
     // message = message.toObject();
-    message.avatar = sender.avatar;
-    message.name = sender.name;
-    message.isMine = message.sender._id.toString() === userId.toString();
+    message.avatar = sender?.avatar;
+    message.name = sender?.name;
+    message.isMine = message.sender?._id.toString() === userId.toString();
 
-    if(message.isDelete){
+    if (message.isDelete) {
       message.content = "This message has been deleted";
     }
 
-    if(message.parent){
+    if (message.parent) {
       message.parent = MessageHelper.generateMessage(message.parent, userId);
     }
 
