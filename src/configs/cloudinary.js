69fb8ad62000bf606f6f19bb6d0f68463d6ea090
hung tang <baohung20020112@gmail.com>
2024-04-14 23:06:59 +0700

    change cloudinary account

--- src/configs/cloudinary.js
+++ src/configs/cloudinary.js
@@ -1,9 +1,12 @@
 const cloudinary = require('cloudinary').v2;
           
 cloudinary.config({ 
-  cloud_name: 'dfrp35blc', 
-  api_key: '198159912466179', 
-  api_secret: '3YH5l_Z9quFXvQzVdUhsOrtVyTE' 
+  // cloud_name: 'dfrp35blc', 
+  // api_key: '198159912466179', 
+  // api_secret: '3YH5l_Z9quFXvQzVdUhsOrtVyTE' 
+  cloud_name: 'qekdev', 
+  api_key: '839287355494943', 
+  api_secret: 'iFSi_nkcaadZziANB8P5leARzZg' 
 });
 
 module.exports = cloudinary;
