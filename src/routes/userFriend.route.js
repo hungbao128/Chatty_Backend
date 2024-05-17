354d76f96bc4aaed2cb2efa8c27dad2b507fd80a
hung tang <baohung20020112@gmail.com>
2024-05-17 18:17:49 +0700

    Fix

--- src/routes/userFriend.route.js
+++ src/routes/userFriend.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/request/:id', catchAsync(authentication), catchAsync(userFriendController.requestAddFriend));
 router.post('/accept/:id', catchAsync(authentication), catchAsync(userFriendController.acceptFriendRequest));
 router.post('/reject/:id', catchAsync(authentication), catchAsync(userFriendController.rejectFriendRequest));
-router.post('/cancel/:id', catchAsync(authentication), catchAsync(userFriendController.rejectFriendRequest));
+router.post('/cancel/:id', catchAsync(authentication), catchAsync(userFriendController.cancelFriendRequest));
 router.post('/remove/:id', catchAsync(authentication), catchAsync(userFriendController.removeFriend));
 router.get('/', catchAsync(authentication), catchAsync(userFriendController.getCurrentUserFriends));
 router.get('/requests', catchAsync(authentication), catchAsync(userFriendController.getFriendRequests));
